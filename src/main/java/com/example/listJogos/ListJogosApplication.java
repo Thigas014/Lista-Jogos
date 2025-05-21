3f0d10802b73f8f4c144115ce28715e96d526af8
Thiago dos Santos Araujo <thiago-araujo.ta@academico.ifpb.edu.br>
2025-05-20 22:20:34 -0300

    commit inicial

---
+++ src/main/java/com/example/listJogos/ListJogosApplication.java
@@ -0,0 +1,13 @@
+package com.example.listJogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListJogosApplication.class, args);
+	}
+
+}
